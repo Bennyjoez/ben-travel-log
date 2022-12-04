ffb8114f126abf0c836515a099615fc6296eb4ca
Benson Njuguna <njugunab655@gmail.com>
2022-12-04 06:40:31 +0300

    Added links to images

---
+++ src/components/sliderData.js
@@ -0,0 +1,127 @@
+export default [
+    {
+        Nairobi : [
+            {
+                image: "https://thumbs.dreamstime.com/b/nairobi-skyline-traffic-to-city-peak-evening-rush-hour-61208570.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/night-view-city-chongqing-china-20796624.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/zebras-nairobi-national-park-nairoby-city-background-zebra-puts-head-back-other-zebra-nairobi-kenya-zebras-128330727.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/nairobi-kenya-june-one-workers-feeding-young-orphant-elephant-milk-sheldrick-orphanage-71216673.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/nairobi-skyline-travel-beyond-boundaries-check-out-46752904.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/nairobi-kenya-july-sculpture-front-national-museum-nairobi-kenya-nairobi-museum-100658859.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/nairobi-12214847.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/africa-children-18505974.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/kenya-parliament-building-28467068.jpg"
+            }
+        ],
+
+        Kisii : [
+            {
+                image: "https://kisiifinest.co.ke/wp-content/uploads/2021/07/Where-is-Kisii-National-Polytechnic-located-directions-map-distance-from-Nairobi-and-contacts.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/kenyan-artisan-carving-design-soapstone-african-artist-pattern-sharp-knife-putting-final-small-painted-box-lid-100562245.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/highlands-kisii-farm-hills-kenya-morning-mixed-farming-practiced-53764816.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/kenyan-black-tea-buds-growing-tea-plant-plantation-kisii-highland-kenya-top-buds-kenyan-black-tea-115036967.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/old-kisii-stone-soapstone-portable-oil-lamp-ethnic-surface-carving-zig-zag-pattern-isolated-white-30044174.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/colorful-finished-african-soapstone-wild-animal-souvenirs-gifts-hand-carved-painted-polished-colors-rainbow-130374927.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/calf-licking-s-nose-zero-grazing-kisii-kenya-53764558.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/african-decoration-beads-accessories-different-colors-common-kenya-maasai-tribe-often-used-to-make-151791472.jpg"
+            }
+            
+        ],
+
+        Mombasa : [
+            {
+                image: "https://thumbs.dreamstime.com/b/city-center-mombasa-kenya-east-africa-84419304.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/mombasa-market-kenya-detail-fruit-vegetables-old-town-40295931.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/indian-ocean-mombasa-seafront-promenade-seaside-pedestrian-area-views-to-poeple-relaxing-friday-afternoon-111827179.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/mombasa-jain-temple-18531601.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/fort-jesus-museum-mombasa-kenya-east-africa-84419284.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/sun-rises-over-scenic-tudor-creek-mombasa-kenya-wooden-jetty-foreground-daybreak-mombasa-167114519.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/container-vessel-under-cargo-operations-port-mombasa-kenya-alongside-port-mombasa-kenya-114782466.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/mombasa-kenya-africa-january-african-women-meet-local-market-near-158036175.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/mombasa-old-town-18559399.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/maasai-warrior-beach-diani-kenya-mombasa-january-145684610.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/sunny-day-famous-giant-elephant-tusks-moi-avenue-mombasa-kenya-sunny-day-famous-giant-elephant-tusks-moi-208055602.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/mombasa-city-old-town-africa-travel-mombasa-city-old-town-africa-travel-use-creative-content-173321033.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/pool-area-diani-reef-beach-spa-resort-mombasa-im-d-tourists-guests-southern-coast-130707726.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/fort-jesus-mombasa-kenya-february-ruins-historic-february-40295873.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/camel-ride-29020820.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/view-indian-ocean-hotel-diani-beach-outside-mombasa-kenya-30162870.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/hand-made-catamaran-3999780.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/kenya-mombasa-starfish-full-colour-ocean-kenya-mombasa-starfish-full-colour-ocean-near-reef-127983005.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/monkey-vervet-tree-eating-mango-park-mombasa-kenya-87599819.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/beautiful-tropical-beach-sunset-orange-tiwi-palm-tree-foreground-mombasa-kenya-75508260.jpg"
+            },
+            {
+                image: "https://thumbs.dreamstime.com/b/luxurious-african-four-star-hotel-lobby-entrance-entrance-view-to-luxurious-lobby-african-four-star-hotel-kenyan-111415411.jpg"
+            },
+        ]
+    }
+]
